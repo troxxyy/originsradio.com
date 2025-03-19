70b2e634df6debd5bb9f950de4a2048a607bbc76
troxxyy <sinacetin@gmail.com>
2025-03-19 14:16:35 +0300

    Add Vercel Analytics for tracking visitor data

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <SpeedInsights />
+    <Analytics />
   </>
 );
