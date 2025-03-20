f796e9f1b61ae0f479c9d44ec6d6fb31aaaa66a3
troxxyy <sinacetin@gmail.com>
2025-03-20 16:30:54 +0300

    Update vite config for better build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig(({ mode }) => ({
   },
   optimizeDeps: {
     include: ['shader-park-core'],
-    //exclude: ['shader-park-core']
+    exclude: ['shader-park-core']
   },
   publicDir: 'public',
   base: '/',
